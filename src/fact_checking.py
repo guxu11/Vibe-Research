8624c340c345d94844a8a674069fc71a69e5fbc9
guxu11 <guxu19970823@126.com>
2025-03-07 14:33:43 -0800

    update script

--- src/fact_checking.py
+++ src/fact_checking.py
@@ -17,6 +17,8 @@ OLLAMA_HOST = sys.argv[2]
 os.environ["OLLAMA_HOST"] = OLLAMA_HOST
 print("OLLAMA_HOST: ", OLLAMA_HOST)
 
+start_index_list = [219, 229, 200, 239, 40]
+
 
 def split_text_into_sentences(text):
     tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
@@ -71,47 +73,45 @@ def write_sentences():
 
 MODEL_OPENAI = "chatgpt-4o-latest"
 MODEL_OLLAMA = "llama3.3:latest"
-def fact_checking_by_type(t, model_family='openai'):
-    print(t)
-    folder = os.path.join(SENTENCE_DIR, t)
-    files = os.listdir(folder)
-    files.sort()
-    for file in files:
-        print(file)
-        try:
-            with open(os.path.join(folder, file), 'r') as f:
-                sentence_dict = json.load(f)
-            raw_text = sentence_dict['raw_text']
-            for model in sentence_dict:
-                if model == 'raw_text':
-                    continue
-                if 'pred_labels' in sentence_dict[model] and 'pred_types' in sentence_dict[model] \
-                    and not (len(sentence_dict[model]['sentences']) > 1 and sentence_dict[model]['pred_labels'] == [0] and sentence_dict[model]['pred_types'] == ['no error']):
-                    continue
-                print(model)
-                sentences = sentence_dict[model]['sentences']
-                prompt = get_fact_checking_prompt(raw_text, sentences)
-                response = ""
-                try:
-                    response = get_response(get_client(), prompt, MODEL_OPENAI) if model_family == 'openai' else get_response_from_ollama(MODEL_OLLAMA, prompt, timeout=90)
-                except Exception as e:
-                    print(e)
-                    continue
-                if response:
-                    pred_labels, pred_types = parsing_llm_fact_checking_output(response)
-                    sentence_dict[model]['pred_labels'] = pred_labels
-                    sentence_dict[model]['pred_types'] = pred_types
-            with open(os.path.join(folder, file), 'w') as f:
-                f.write(json.dumps(sentence_dict))
-        except Exception as e:
-            print(e)
-            continue
+def fact_checking_by_type(model_family='openai'):
+    for i, t in enumerate(TEXT_CATEGORIES):
+        print(t)
+        start_index = start_index_list[i]
+        folder = os.path.join(SENTENCE_DIR, t)
+        files = os.listdir(folder)
+        files.sort()
+        files = files[start_index+task_id::4]
+        print(files)
+        for file in files:
+            print(f'{t}-{file}')
+            try:
+                with open(os.path.join(folder, file), 'r') as f:
+                    sentence_dict = json.load(f)
+                raw_text = sentence_dict['raw_text']
+                for model in sentence_dict:
+                    if model == 'raw_text':
+                        continue
+                    print(model)
+                    sentences = sentence_dict[model]['sentences']
+                    prompt = get_fact_checking_prompt(raw_text, sentences)
+                    response = ""
+                    try:
+                        response = get_response(get_client(), prompt, MODEL_OPENAI) if model_family == 'openai' else get_response_from_ollama(MODEL_OLLAMA, prompt, timeout=90)
+                    except Exception as e:
+                        print(e)
+                        continue
+                    if response:
+                        pred_labels, pred_types = parsing_llm_fact_checking_output(response)
+                        sentence_dict[model]['pred_labels'] = pred_labels
+                        sentence_dict[model]['pred_types'] = pred_types
+                with open(os.path.join(folder, file), 'w') as f:
+                    f.write(json.dumps(sentence_dict))
+            except Exception as e:
+                print(e)
+                continue
+        print(f'{t} completed. {i + 1}/{len(TEXT_CATEGORIES)}')
 
 
 if __name__ == '__main__':
-    if task_id in (0, 1, 2):
-        fact_checking_by_type(TEXT_CATEGORIES[task_id], 'ollama')
-    else:
-        for i in range(3,5):
-            fact_checking_by_type(TEXT_CATEGORIES[i], 'ollama')
+    fact_checking_by_type('ollama')
     print("🎉 All tasks completed!")
